0ea744ba06514af7c262e07aac2eeaddebb63ebc
MohamedElsay0d <me827794@gmail.com>
2024-09-07 08:54:49 -0700

    changes in api and repo

--- lib/Data/repositry/breakingbad_repo.dart
+++ lib/Data/repositry/breakingbad_repo.dart
@@ -3,8 +3,10 @@ import 'package:character_cubit_api/Data/models/Character.dart';
 import '../web_services/breakingbad_api.dart';
 
 class BreakingbadRepo {
+  final BreakingBadApi breakingBadApi;
+  BreakingbadRepo({required this.breakingBadApi});
   Future<List<Character>> getCharacters() async {
-    final response = await BreakingBadApi().getCharacters();
+    final response = await breakingBadApi.getCharacters();
     return response.map((e) => Character.fromJson(e)).toList();
   }
 }
--- lib/Data/web_services/breakingbad_api.dart
+++ lib/Data/web_services/breakingbad_api.dart
@@ -4,13 +4,17 @@ import '../../constant/Strings.dart';
 
 class BreakingBadApi {
   late Dio dio;
-  BaseOptions options = BaseOptions(
-    baseUrl: baseUrl,
-    receiveDataWhenStatusError: true,
-    connectTimeout: const Duration(seconds: 20),
-    receiveTimeout: const Duration(seconds: 20),
-    sendTimeout: const Duration(seconds: 20),
-  );
+
+  BreakingBadApi() {
+    BaseOptions options = BaseOptions(
+      baseUrl: baseUrl,
+      receiveDataWhenStatusError: true,
+      connectTimeout: const Duration(seconds: 20),
+      receiveTimeout: const Duration(seconds: 20),
+      sendTimeout: const Duration(seconds: 20),
+    );
+    dio = Dio(options);
+  }
   Future<List<dynamic>> getCharacters() async {
     final response = await dio.get('character');
     return response.data;
